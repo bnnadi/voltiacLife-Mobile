bc1ad527b4c8d4b2e8a38b5a4ca685dbcc4a06eb
Bisike <bisike.nnadi@gmail.com>
2013-12-11 00:07:33 -0500

    [Delivers #62091068] was able to save some of the facebook open graph infomation to firebase, still working on the users likes and their friends likes

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,45 @@
 var voltaicLifeApp = angular.module('voltaicLife', ['firebase', 'ngRoute']);
-var URL = "https://voltaiclife.firebaseio.com/users";
 
-voltaicLifeApp.run(['$firebaseAuth', '$rootScope', function ($firebaseAuth, $rootScope) {
+voltaicLifeApp.run(['$firebaseAuth', '$rootScope', '$firebase', function ($firebaseAuth, $rootScope, $firebase) {
+    var URL = "https://voltaiclife.firebaseio.com/";
     var ref = new Firebase(URL);
     $rootScope.auth = $firebaseAuth(ref);
+    
+    $rootScope.$on("$firebaseAuth:login", function(evt, user){
+        console.log("User " + user.id + " successfully logged in!!!");
+        
+        $rootScope.user = $firebase(new Firebase(URL+ 'users/' + user.id));
+        
+        $rootScope.user.$on("loaded", function(userData){
+            console.log("user data loaded");
+            console.log("userData ", userData);
+            
+//            console.log("user likes", userLikes);
+            console.log("user ", user);
+            if(!userData){
+                var newUser = {
+                    name: user.name,
+                    gender: user.gender,
+                    username: user.username,
+                    location: user.location.name,
+                    image:  "http://graph.facebook.com/" + user.username + "/picture"
+                }
+                
+                $rootScope.user.$set(newUser);
+            }
+        });
+        
+    });
+    
+    
+    $rootScope.$on("$firebaseAuth:logout", function(evt){
+          console.log("User successfully logged out, sadness");
+    });
+    
+    $rootScope.$on("$firebaseAuth:error", function(evt, err){
+        console.log("Major error to report");
+    });
+    
 }]);    
 
 voltaicLifeApp.config(function($routeProvider){
--- app/js/controllers/login_controller.js
+++ app/js/controllers/login_controller.js
@@ -4,21 +4,10 @@
 "use strict";
 
 voltaicLifeApp.controller('login', ['$scope', '$firebaseAuth', function($scope, $firebaseAuth){
-    
-    $scope.$on("$firebaseAuth:login", function(evt, user){
-        console.log("User " + user.id + " successfully logged in!!!");
-    });
-    
-    $scope.$on("$firebaseAuth:logout", function(evt){
-          console.log("User successfully logged out, sadness");
-    });
-    
-    $scope.$on("$firebaseAuth:error", function(evt, err){
-        console.log("Major error to report");
-    });
 
     $scope.$login = function(){
-        $firebaseAuth.$login("facebook");
+        $firebaseAuth.$login('facebook',{scope: 'user_likes, friends_likes, user_interests, friends_interests'
+        });
         console.log("Logging into facebook");
     };
 
